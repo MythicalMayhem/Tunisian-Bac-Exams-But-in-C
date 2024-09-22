8eaf009629cd0312f2e503f6a0f84b49b17ba3ce
MythicalMayhem <ameuroussema@gmail.com>
2024-09-22 22:52:31 +0100

    bac 2024

--- principale 2024/ex4.c
+++ principale 2024/ex4.c
@@ -1,6 +1,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+struct Inv {
+    char *nom;
+    int score;
+};
+
 char *Lire_Ligne(FILE *f, char **p) {
     int c;
     int count = 0;
@@ -24,24 +30,49 @@ char *Lire_Ligne(FILE *f, char **p) {
     }
     fgets(s, sizeof(char) * count, f);
     getc(f);  // for the \n
- 
     *p = s;
     return s;
 }
 
-void main() {
-    FILE *Finv = fopen("Invites.txt", "r");
+bool Triangulaire(int Nb) {
+    int N = 1;
+    while (N * (N + 1) / 2 < Nb) N++;
+    return (N * (N + 1) / 2 == Nb);
+};
+int Calcul_Score(char *ch) {
+    int s = 0;
+    for (; *ch != '\0'; ch++) s = s + (int)*ch - 64;
+    return s;
+}
 
+int Max_Score(struct Inv T[], int N) {
+    int Max = T[0].score;
+    for (int i = 0; i < N; i++)
+        if (T[i].score > Max) Max = T[i].score;
+    return Max;
+};
+void Affice_Gangnats(struct Inv T[], int N) {
+    int Max = Max_Score(T, N);
+    for (int i = 0; i < N; i++)
+        if (T[i].score = Max) printf("le gangnat est %s", T[i].nom);
+}
+void TransfertNoms(struct Inv T[], int N) {
+    FILE *Finv = fopen("Invites.txt", "r");
     while (!feof(Finv)) {
         char *ret;
         Lire_Ligne(Finv, &ret);
-        printf("%s \n", ret);
-        free(ret);
-        // getLine(Finv, s, lineLength);
-    }
-    // for last line
+        printf("%d ", strlen(ret));
 
-    // getLine(Finv, s, lineLength);
+        struct Inv E;
+        E.nom = ret;
+        E.score = Calcul_Score(E.nom);
+
+        if (Triangulaire(E.score)) {
+        }
+
+        printf("%s \n", E.nom, E.score);
+        free(ret);
+    }
     fclose(Finv);
 }
 
